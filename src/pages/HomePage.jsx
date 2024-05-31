3c648e96e008d51ee0d00fb4bc3cbf37ce78b3e9
vivalafreak1 <arieftaufikrahman123@gmail.com>
2024-05-31 21:54:57 +0700

    Styling HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,31 +1,42 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import galleryImages from "../utils/galery";
 
 export default function HomePage() {
   useEffect(() => {
-    document.title = "Destination App";
+    document.title = "Explore Bali";
   }, []);
 
   return (
     <main>
       <div
         id="description"
-        className="flex flex-col items-center justify-center h-screen text-center text-white bg-fixed bg-gray-800 bg-center bg-cover"
+        className="flex flex-col items-center justify-center h-screen p-6 text-center text-black bg-fixed bg-white bg-center bg-cover"
       >
-        <div>
-          <h2 className="mb-4 text-4xl font-bold">TOURISM DESTINATION APP</h2>
-        </div>
-        <div>
-          <div className="text-lg">
-            <span className="block text-2xl font-semibold">
-              DESTINATION APP
-            </span>
-            <span>
-              {" "}
-              helps you find various types of amazing{" "}
-              <b>tourist destinations</b> to meet your <b>adventure</b> and{" "}
-              <b>travel</b> needs.{" "}
-            </span>
+        <div className="container mx-auto">
+          <div className="grid items-center grid-cols-1 gap-8 md:grid-cols-2">
+            <div>
+              <h2 className="mb-4 text-4xl font-bold leading-tight">
+                TOURISM DESTINATION APP
+              </h2>
+              <p className="text-lg leading-relaxed">
+                <span className="block mb-2 text-2xl font-semibold">
+                  DESTINATION APP
+                </span>
+                <span>
+                  helps you find various types of amazing{" "}
+                  <b>tourist destinations</b> to meet your <b>adventure</b> and{" "}
+                  <b>travel</b> needs.
+                </span>
+              </p>
+            </div>
+            <div className="flex justify-center">
+              <img
+                className="w-full h-auto rounded-lg shadow-lg"
+                src="https://ik.imagekit.io/tvlk/blog/2023/09/shutterstock_631736717.jpg"
+                alt="Tourism Destination"
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -35,21 +46,26 @@ export default function HomePage() {
         className="flex flex-col items-center justify-center h-screen py-20 text-center text-gray-800 bg-fixed bg-gray-100 bg-center bg-cover"
       >
         <div>
-          <h2 className="mb-4 text-4xl font-bold">SERVICES</h2>
+          <h2 className="mb-4 text-4xl font-bold text-indigo-700">SERVICES</h2>
         </div>
         <div className="w-full max-w-4xl">
           <div className="mb-8">
-            <p className="text-lg">
+            <p className="text-lg leading-relaxed text-gray-700">
               <b>Destination App</b> provides various features that can help you
               find amazing travel ideas according to your wishes.
             </p>
-            <Link to="/destinations" className="text-blue-500 hover:underline">
+            <Link
+              to="/destinations"
+              className="text-blue-500 hover:text-indigo-700 hover:underline"
+            >
               Try our service
             </Link>
           </div>
           <div className="flex flex-wrap justify-around">
             <div className="w-full p-4 service md:w-1/2">
-              <h3 className="mb-2 text-2xl font-bold">Destination List</h3>
+              <h3 className="mb-2 text-2xl font-bold text-indigo-700">
+                Destination List
+              </h3>
               <div className="flex justify-center mb-2">
                 <img
                   className="w-24 h-24"
@@ -57,14 +73,16 @@ export default function HomePage() {
                   alt="placeholder"
                 />
               </div>
-              <p>
+              <p className="text-gray-700">
                 The <b>Destination List</b> feature provides a random list of
                 amazing tourist spots, ranging from mountains, beaches, and
                 historical sites.
               </p>
             </div>
             <div className="w-full p-4 service md:w-1/2">
-              <h3 className="mb-2 text-2xl font-bold">Search Destinations</h3>
+              <h3 className="mb-2 text-2xl font-bold text-indigo-700">
+                Search Destinations
+              </h3>
               <div className="flex justify-center mb-2">
                 <img
                   className="w-24 h-24"
@@ -72,12 +90,32 @@ export default function HomePage() {
                   alt="placeholder"
                 />
               </div>
-              <p>
+              <p className="text-gray-700">
                 The <b>Search Destinations</b> feature provides a list of
                 tourist spots according to the desired location, type, or
                 activities available.
               </p>
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        id="gallery"
+        className="flex flex-col items-center justify-center py-8 text-center bg-fixed bg-white bg-center bg-cover"
+      >
+        <div className="container mx-auto">
+          <h2 className="mb-4 text-4xl font-bold text-indigo-700">GALLERY</h2>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+            {galleryImages.map((src, index) => (
+              <div key={index} className="p-2">
+                <img
+                  className="object-cover w-full h-auto rounded-lg shadow-lg md:h-64"
+                  src={src}
+                  alt={`Gallery image ${index + 1}`}
+                />
+              </div>
+            ))}
           </div>
         </div>
       </div>
---
+++ src/utils/galery.js
@@ -0,0 +1,10 @@
+const galleryImages = [
+  "https://www.travelandleisure.com/thmb/KE0vV7K8Ngvc_7j-_FGx_jCUdCM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-bali-lead-image-BALITG1223-101f43c88c044081a4558b737afbd094.jpg",
+  "https://ikpi.or.id/wp-content/uploads/2023/07/bali.jpg",
+  "https://content.api.news/v3/images/bin/0a4b0d4fa1be3bf4c69500972c6d7880",
+  "https://digital.ihg.com/is/image/ihg/intercontinental-bali-8338027557-4x3",
+  "https://api.time.com/wp-content/uploads/2022/07/Worlds-Greatest-Places-2022-BaliIndonesia.jpeg",
+  "https://static.independent.co.uk/2023/06/02/12/iStock-1157937978.jpg",
+];
+
+export default galleryImages;

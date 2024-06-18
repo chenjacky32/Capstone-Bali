9c540605e0d9c31ab4b8170ae08e1be1778490f6
vivalafreak1 <arieftaufikrahman123@gmail.com>
2024-06-18 12:58:18 +0700

    remove lazy load

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,35 @@
-import React, { Suspense, useState, useEffect } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import PrivateRoute from "./utils/PrivateRoute";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import LoadingSpinner from "./components/LoadingSpinner";
-
-const HomePage = React.lazy(() => import("./pages/HomePage"));
-const DestinationPage = React.lazy(() => import("./pages/DestinationPage"));
-const LoginPage = React.lazy(() => import("./pages/LoginPage"));
-const DashboardPage = React.lazy(() => import("./pages/DashboardPage"));
-const DetailPage = React.lazy(() => import("./pages/DetailPage"));
-const NotFoundPage = React.lazy(() => import("./pages/NotFoundPage"));
+import HomePage from "./pages/HomePage"; // Import components directly (not lazy loaded)
+import DestinationPage from "./pages/DestinationPage";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
+import DetailPage from "./pages/DetailPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate a delay for loading purposes
-    const timeout = setTimeout(() => {
-      setLoading(false);
-    }, 2000); // Adjust the delay time as needed
-
-    return () => clearTimeout(timeout);
-  }, []);
-
   return (
     <div className="flex flex-col min-h-screen bg-color-dark">
       <NavBar />
       <main className="flex-grow">
-        <Suspense fallback={loading ? <LoadingSpinner /> : null}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/destinations" element={<DestinationPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/destinations/:id" element={<DetailPage />} />
-            <Route
-              path="/dashboard"
-              element={
-                <PrivateRoute>
-                  <DashboardPage />
-                </PrivateRoute>
-              }
-            />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/destinations" element={<DestinationPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/destinations/:id" element={<DetailPage />} />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <DashboardPage />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
       </main>
       <Footer />
     </div>
